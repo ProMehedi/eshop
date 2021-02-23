aaadd43d4c56abc2be6c49b51377e3696417f24f
promehedi <mmhasan563@gmail.com>
2021-02-23 20:48:45 +0600

    Post a Product

--- routers/productRouters.js
+++ routers/productRouters.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { Category } from '../models/categoryModel.js'
 import { Product } from '../models/productModel.js'
 
 const productRouters = express.Router()
@@ -17,25 +18,33 @@ productRouters.get('/', async (req, res) => {
 })
 
 // App New Product
-productRouters.post('/', (req, res) => {
+productRouters.post('/', async (req, res) => {
+  const category = await Category.findById(req.body.category)
+  if (!category) return res.status(400).send('Invalid Cateogry')
+
   const product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    images: req.body.images,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   })
 
   // Save to MongoDB
-  product
-    .save()
-    .then((createdPorudct) => {
-      res.status(201).json(createdPorudct)
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error,
-        succuss: false,
-      })
-    })
+  const updatedProduct = await product.save()
+
+  if (!updatedProduct) {
+    return res.status(400).send("This product can't be created!")
+  } else {
+    return res.status(201).send(updatedProduct)
+  }
 })
 
 export default productRouters
