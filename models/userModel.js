63d2a82be26826e6f66176d3ac85916b25c05204
promehedi <mmhasan563@gmail.com>
2021-02-24 04:21:14 +0600

    User Model & Schema

--- models/userModel.js
+++ models/userModel.js
@@ -2,14 +2,26 @@ import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
   {
-    name: String,
-    image: String,
-    countInStock: {
-      type: Number,
-      default: 0,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    passwordHash: { type: String, required: true },
+    phone: { type: String, required: true },
+    isAdmin: { type: String, default: false },
+    street: { type: String, default: '' },
+    apartment: { type: String, default: '' },
+    zip: { type: String, default: '' },
+    city: { type: String, default: '' },
+    country: { type: String, default: '' },
   },
   { timestamps: true }
 )
+
+// Duplicate the ID field.
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Ensure virtual fields are serialised.
+userSchema.set('toJSON', { virtuals: true })
 
 export const User = mongoose.model('User', userSchema)
